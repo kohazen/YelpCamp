0347599384cc3f880d04937e4db62f26a08fe377
kohazen <kohazendeveloper.com>
2024-04-25 21:13:15 +0530

    updated app.js and campground.js

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const Campground = require('./models/campground');
+
 //path module, a built-in module in Node.js 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+//Assigns the connection object from the imported mongoose library to a constant named db. 
+//The connection object represents the established connection to your MongoDB database
+db.on("error",console.error.bind(console,"connection error:"));
+//sets up an event listener on the db object,  listens for the "error" event emitted by the connection.
+//creates a function using the .bind() method. This method is used to pre-configure the function's this context 
+//(in this case, set to console) and arguments (the error message "connection error:"). 
+db.once("open",()=>{
+    console.log("Database connected");
+})
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'))//need to require path module to use path
 
 app.get('/',(req,res)=>{
     res.render('home');
+})
+
+app.get('/makecampground', async (req,res)=>{
+    const camp = new Campground({title:"My Backyard",description:'Cheap Camping'});
+    await camp.save();
+    res.send(camp);
 })
 
 
--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,15 @@
+//we create and save database schema in a seperate file;
+//it is for security perposes;
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const CampgroundSchema = new Schema({
+    title:String,
+    price:String,
+    description:String,
+    location:String
+});
 
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+module.exports = mongoose.model('Campground',CampgroundSchema);
+//we made a model from the schema and named it "Campground"
